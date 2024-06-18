98a75a250f23eea1a5d75290030e76d81f6c48a5
HeberthDev <heberthdeveloper@gmail.com>
2024-06-18 08:19:04 -0300

    Inserindo objeto de áudio

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ const banner = document.querySelector('.app__image');
 
 const titulo = document.querySelector('.app__title');
 
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('./sons/luna-rise-part-one.mp3');
+musica.loop = true;
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+});
+
 focoBtn?.addEventListener('click', () => {
     alterarContexto('foco');
     focoBtn.classList.add('active');
